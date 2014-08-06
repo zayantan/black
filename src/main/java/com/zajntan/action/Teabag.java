12f006ab5a7deec847d778e1968ea161d832ba78
Sayantan Ghosh <zayantan@gmail.com>
2014-08-06 04:22:19 +0100

    beta stable
    TODO: wire struts2

---
+++ src/main/java/com/zajntan/action/Teabag.java
@@ -0,0 +1,23 @@
+package com.zajntan.action;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.zajntan.model.MessageHibModel;
+
+@Stateless
+public class Teabag {
+	@PersistenceContext
+    private EntityManager em;
+
+	MessageHibModel MessageHib = new MessageHibModel(
+			"Hello World with JPA and JTA");
+	
+    @SuppressWarnings("unchecked")
+    public List<EntityManager> get() {
+        return em.createNativeQuery("select * from MESSAGES", MessageHibModel.class).getResultList();
+    }
+}
